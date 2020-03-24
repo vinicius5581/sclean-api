0e765c16d862c8bdb319e2bc14a31e41114f83be
Vinicius Santana <santanaviny@gmail.com>
2020-03-23 17:30:25 -0700

    Prettier

--- app.js
+++ app.js
@@ -8,41 +8,39 @@ const passport = require('passport');
 /**
  * Database connection
  */
-const MongoStore = require('connect-mongo')(session);
 const connection = require('./config/database');
-
+const MongoStore = require('connect-mongo')(session);
 
 const routes = require('./routes');
 
 /**
  * -------------- GENERAL SETUP ----------------
  */
-console.log('test')
+console.log('test');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * -------------- SESSION SETUP ----------------
  */
 
-const sessionStore = new MongoStore({ 
-    mongooseConnection: connection, 
-    collection: 'sessions' 
+const sessionStore = new MongoStore({
+  mongooseConnection: connection,
+  collection: 'sessions',
 });
 
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
-    }
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+  },
 };
 
 app.use(session(sessionOptions));
@@ -64,24 +62,24 @@ app.use(passport.session());
 app.use(routes);
 
 app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    next(error);
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 /**
  * -------------- APP LISTEN ----------------
  */
 
 app.listen(process.env.PORT, () => {
-    console.log('to rodando');
+  console.log('to rodando');
 });
